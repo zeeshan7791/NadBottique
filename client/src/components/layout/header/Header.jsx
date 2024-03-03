7d9c06f766841ea02ccdf45244a4b90bdb921416
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-03 21:15:44 +0500

    header and footer created

---
+++ client/src/components/layout/header/Header.jsx
@@ -0,0 +1,53 @@
+import { ReactNavbar } from "overlay-navbar";
+import logo from "../../../assets/logo/logo.png";
+import { MdAccountCircle } from "react-icons/md";
+import { MdSearch } from "react-icons/md";
+import { FiShoppingBag } from "react-icons/fi";
+
+const Header = () => {
+  const options = {
+    burgerColorHover: "#eb4034",
+    logo,
+    logoWidth: "20vmax",
+    navColor1: "white",
+    logoHoverSize: "10px",
+    logoHoverColor: "#eb4034",
+    link1Text: "Home",
+    link2Text: "Products",
+    link3Text: "Contact",
+    link4Text: "About",
+    link1Url: "/",
+    link2Url: "/products",
+    link3Url: "/contact",
+    link4Url: "/about",
+    link1Size: "1.3vmax",
+    link1Color: "rgba(35, 35, 35,0.8)",
+    nav1justifyContent: "flex-end",
+    nav2justifyContent: "flex-end",
+    nav3justifyContent: "flex-start",
+    nav4justifyContent: "flex-start",
+    link1ColorHover: "#eb4034",
+    link1Margin: "1vmax",
+    profileIconColorHover: "#eb4034",
+    searchIconColorHover: "#eb4034",
+    cartIconColorHover: "#eb4034",
+    cartIconMargin: "1vmax",
+    profileIconUrl: "/login",
+    profileIcon: true,
+    profileIconColor: "rgba(35, 35, 35,0.8)",
+    ProfileIconElement: MdAccountCircle,
+    searchIcon: true,
+    searchIconColor: "rgba(35, 35, 35,0.8)",
+    SearchIconElement: MdSearch,
+    cartIcon: true,
+    cartIconColor: "rgba(35, 35, 35,0.8)",
+    CartIconElement: FiShoppingBag,
+  };
+  return (
+    <div>
+      <ReactNavbar {...options} />
+    </div>
+  );
+};
+
+export default Header;
