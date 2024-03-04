48a201900f8003f225229b931dd650555a7e6e96
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-04 20:45:50 +0500

    home page created

--- client/src/Pages/home/Home.jsx
+++ client/src/Pages/home/Home.jsx
@@ -1,11 +1,18 @@
-import React from "react";
 import "./Home.css";
 // import {CgMouse} from "react-icons/all"
 import { CgMouse } from "react-icons/cg";
+import ProductCard from "../../components/products/ProductCard";
+import MetaData from "../../components/layout/MetaData";
 const Home = () => {
+  const product = {
+    name: "car",
+    description: "brand new car",
+    price: 500,
+    _id: "1",
+  };
   return (
     <>
-      {" "}
+      <MetaData title={"Nad Boutique"} />
       <div className="banner">
         <p>Welcome to Ecommerce</p>
         <h1>FIND AMAZING PRODUCTS BELOW</h1>
@@ -17,6 +24,16 @@ const Home = () => {
         </a>
       </div>
       <h2 className="homeHeading">Featured Products</h2>
+      <div className="container" id="container">
+        <ProductCard key={product._id} product={product} />
+        <ProductCard key={product._id} product={product} />
+        <ProductCard key={product._id} product={product} />
+        <ProductCard key={product._id} product={product} />
+        <ProductCard key={product._id} product={product} />
+        <ProductCard key={product._id} product={product} />
+        <ProductCard key={product._id} product={product} />
+        <ProductCard key={product._id} product={product} />
+      </div>
     </>
   );
 };
