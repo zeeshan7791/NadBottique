bc2709a9f6d1f60c55843702fc02ad2f9d119411
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-10 16:31:21 +0500

    product reduced functionality created

--- client/src/redux/reducers/productReducer.js
+++ client/src/redux/reducers/productReducer.js
@@ -3,28 +3,26 @@ import {
   ALL_PRODUCT_SUCCESS,
   ALL_PRODUCT_FAIL,
 } from "../constants/productConstants";
-export const productReducer =
-  ((state = { products: [] }),
-  (action) => {
-    switch (action.type) {
-      case ALL_PRODUCT_REQUEST:
-        return {
-          loading: true,
-          product: [],
-        };
-      case ALL_PRODUCT_SUCCESS:
-        return {
-          loading: false,
-          product: action.payload.products,
-          productsCount: action.payload.productsCount,
-        };
-      case ALL_PRODUCT_FAIL:
-        return {
-          loading: false,
-          error: action.payload,
-        };
+export const productReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case ALL_PRODUCT_REQUEST:
+      return {
+        loading: true,
+        products: [],
+      };
+    case ALL_PRODUCT_SUCCESS:
+      return {
+        loading: false,
+        products: action.payload.products,
+        productsCount: action.payload.productsCount,
+      };
+    case ALL_PRODUCT_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
 
-      default:
-        return state;
-    }
-  });
+    default:
+      return state;
+  }
+};
