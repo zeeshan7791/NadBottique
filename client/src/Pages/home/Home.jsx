cfd1a8a62d6a738b6f467dd561c6d735bec4d322
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-16 16:13:44 +0500

    product fetched functionality fixed

--- client/src/Pages/home/Home.jsx
+++ client/src/Pages/home/Home.jsx
@@ -3,29 +3,33 @@ import "./Home.css";
 import { CgMouse } from "react-icons/cg";
 import ProductCard from "../../components/products/ProductCard";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { allProductsActions } from "../../redux/products/allProductsSlice";
-import { useEffect } from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
+// import axios from "axios";
 import MetaData from "../../components/layout/MetaData";
-import { Link } from "react-router-dom";
+
+import Loader from "../../components/layout/loader/Loader";
+import { toast } from "react-toastify";
 const Home = () => {
   const dispatch = useDispatch();
-
+  const [loading, setLoading] = useState(false);
   const showAllProducts = async () => {
     try {
       dispatch(allProductsActions.ALL_PRODUCT_REQUEST());
-      const data = await axios.get(
-        "http://localhost:5000/api/product/all-products"
-      );
-
+      setLoading(true);
+      const res = await fetch("http://localhost:5000/api/product/all-products");
+      const data = await res.json();
       console.log(data, "value in data------");
       if (data.success === false) {
         dispatch(allProductsActions.ALL_PRODUCT_FAIL(data.message));
         return;
       }
       dispatch(allProductsActions.ALL_PRODUCT_SUCCESS(data));
+      toast.success(data.message);
+      setLoading(false);
     } catch (error) {
+      setLoading(false);
       dispatch(allProductsActions.ALL_PRODUCT_FAIL(error.message));
     }
   };
@@ -50,7 +54,7 @@ const Home = () => {
       </div>
       <h2 className="homeHeading">Featured Products</h2>
       <div className="container" id="container">
-        <ProductCard />
+        {loading ? <Loader /> : <ProductCard />}
       </div>
     </>
   );
