832d3e2bdfcf00076ae2769f9ac0e922fe6dfdcc
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-16 16:13:01 +0500

    product details redux state managed

---
+++ client/src/redux/products/productDetailsSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+} from "../constants/productConstants";
+
+const productDetailsSlice = createSlice({
+  name: "productDetails",
+  initialState: { productDetails: null, isError: null, isLoading: false },
+  reducers: {
+    [PRODUCT_DETAILS_REQUEST]: (state) => {
+      state.isLoading = true;
+    },
+    [PRODUCT_DETAILS_SUCCESS]: (state, action) => {
+      state.productDetails = action.payload;
+      state.isError = null;
+      state.isLoading = false;
+    },
+    [PRODUCT_DETAILS_FAIL]: (state, action) => {
+      (state.isError = action.payload), (state.isLoading = null);
+    },
+  },
+});
+export const productDetailsAction = productDetailsSlice.actions;
+
+export default productDetailsSlice.reducer;
