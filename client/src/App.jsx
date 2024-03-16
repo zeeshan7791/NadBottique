00513db0cb7b13a87a02749b6846dd511b2f1324
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-16 16:17:08 +0500

    product details route created and toast container added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import WebFont from "webfontloader";
 import Header from "./components/layout/header/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Home from "./Pages/home/Home";
 import Footer from "./components/layout/footer/Footer";
 import LoginSignUp from "./Pages/loginSignUp/Auth";
+import ProductDetails from "./components/products/ProductDetails";
 
 const App = () => {
   useEffect(() => {
@@ -19,8 +22,10 @@ const App = () => {
     <>
       <BrowserRouter>
         <Header />
+        <ToastContainer />
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/product/:id" element={<ProductDetails />}></Route>
           <Route path="/auth" element={<LoginSignUp />}></Route>
         </Routes>
         <Footer />
