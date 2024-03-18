002e0d53731a1c67c38279a2fdf2c0824d86516b
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-18 16:07:23 +0500

    product Card functionality updated

--- client/src/components/products/ProductCard.jsx
+++ client/src/components/products/ProductCard.jsx
@@ -1,66 +1,48 @@
 import { Link } from "react-router-dom";
-// import PropTypes from "prop-types";
-import { useDispatch, useSelector } from "react-redux";
-import { productDetailsAction } from "../../redux/products/productDetailsSlice";
-import { toast } from "react-toastify";
+import PropTypes from "prop-types";
 
-const ProductCard = () => {
-  const { allProducts } = useSelector((state) => state.allProducts);
-  const { productDetails } = useSelector((state) => state.productDetails);
-  const dispatch = useDispatch();
+import { Rating } from "@material-ui/lab";
+import { imageLink } from "../../config/config";
 
-  const showProductDetails = async (productId) => {
-    try {
-      dispatch(productDetailsAction.PRODUCT_DETAILS_REQUEST());
-
-      const res = await fetch(
-        `http://localhost:5000/api/product/single-product/${productId}`
-      );
-      const data = await res.json();
-      console.log(data, "value in data------");
-      if (data.success === false) {
-        dispatch(productDetailsAction.PRODUCT_DETAILS_FAIL(data.message));
-        return;
-      }
-      dispatch(productDetailsAction.PRODUCT_DETAILS_SUCCESS(data));
-      toast.success(data.message);
-    } catch (error) {
-      dispatch(productDetailsAction.ALL_PRODUCT_FAIL(error.message));
-    }
+const ProductCard = ({ product }) => {
+  const options = {
+    value: product.ratings,
+    readOnly: true,
+    precision: 0.5,
   };
-  console.log(productDetails, "productDetails----------");
+
   return (
     <>
-      {allProducts.products.map((product) => (
-        <Link
-          key={product._id}
-          onClick={() => showProductDetails(product._id)}
-          className="productCard"
-          to={`/product/${product._id}`}
-        >
-          <p>{product.name}</p>
-          <p>{product.description}</p>
+      <Link
+        key={product._id}
+        className="productCard"
+        to={`/product/${product._id}`}
+      >
+        <img src={imageLink + product.pictures[0]} alt={product.name} />
+        <p>{product.name}</p>
+        <p>{product.description}</p>
 
-          <div>
-            <span className="productCardSpan"></span>
-          </div>
-          <span>{`₹${product.price}`}</span>
-        </Link>
-      ))}
+        <div>
+          <Rating {...options} /> <span className="productCardSpan"></span>
+        </div>
+        <span>{`₹${product.price}`}</span>
+      </Link>
     </>
   );
 };
 
-// ProductCard.propTypes = {
-//   allProducts: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       _id: PropTypes.string.isRequired,
-//       name: PropTypes.string.isRequired,
-//       description: PropTypes.string.isRequired,
-//       price: PropTypes.number.isRequired,
-//       // Add more properties as needed
-//     })
-//   ).isRequired,
-// };
+ProductCard.propTypes = {
+  product: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      pictures: PropTypes.array.isRequired,
+      price: PropTypes.number.isRequired,
+      Rating: PropTypes.string.isRequired,
+      // Add more properties as needed
+    })
+  ).isRequired,
+};
 
 export default ProductCard;
