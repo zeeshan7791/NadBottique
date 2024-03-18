8dd0b927627d152970ab0725d33cabab2e3d4fd7
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-18 15:44:41 +0500

    Feature product functionality completed

--- client/src/Pages/home/Home.jsx
+++ client/src/Pages/home/Home.jsx
@@ -3,22 +3,26 @@ import "./Home.css";
 import { CgMouse } from "react-icons/cg";
 import ProductCard from "../../components/products/ProductCard";
 
-import { useDispatch } from "react-redux";
-import { allProductsActions } from "../../redux/products/allProductsSlice";
-import { useEffect, useState } from "react";
 // import axios from "axios";
 import MetaData from "../../components/layout/MetaData";
 
 import Loader from "../../components/layout/loader/Loader";
-import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { allProductsActions } from "../../redux/products/allProductsSlice";
+import { useEffect } from "react";
+import { serverURL } from "../../config/config";
+
 const Home = () => {
+  const { allProducts, loading, error } = useSelector(
+    (state) => state.allProducts
+  );
+  console.log(allProducts, "value in all products");
   const dispatch = useDispatch();
-  const [loading, setLoading] = useState(false);
   const showAllProducts = async () => {
     try {
       dispatch(allProductsActions.ALL_PRODUCT_REQUEST());
-      setLoading(true);
-      const res = await fetch("http://localhost:5000/api/product/all-products");
+      const res = await fetch(`${serverURL}/product/all-products`);
+
       const data = await res.json();
       console.log(data, "value in data------");
       if (data.success === false) {
@@ -26,10 +30,7 @@ const Home = () => {
         return;
       }
       dispatch(allProductsActions.ALL_PRODUCT_SUCCESS(data));
-      toast.success(data.message);
-      setLoading(false);
     } catch (error) {
-      setLoading(false);
       dispatch(allProductsActions.ALL_PRODUCT_FAIL(error.message));
     }
   };
@@ -37,7 +38,6 @@ const Home = () => {
   useEffect(() => {
     showAllProducts();
   }, [dispatch]);
-
   return (
     <>
       <MetaData title={"Nadboutique"} />
@@ -54,7 +54,16 @@ const Home = () => {
       </div>
       <h2 className="homeHeading">Featured Products</h2>
       <div className="container" id="container">
-        {loading ? <Loader /> : <ProductCard />}
+        {error ? (
+          "error"
+        ) : loading ? (
+          <Loader />
+        ) : (
+          allProducts.products &&
+          allProducts.products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))
+        )}
       </div>
     </>
   );
