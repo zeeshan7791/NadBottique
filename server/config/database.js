af69c46cae196edb93652534ec79584c86065860
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-21 22:52:22 +0500

    db link fixed

--- server/config/database.js
+++ server/config/database.js
@@ -1,17 +1,31 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
 
-const connectDB = () => {
-  mongoose
-    .connect(process.env.MONGOURL, {
+// const connectDB = () => {
+//   mongoose
+//     .connect(process.env.MONGOURL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     })
+//     .then((con) => {
+//       console.log(`connect to mongoDB ${con.connection.host}`.bgCyan);
+//     })
+//     .catch((error) => {
+//       console.log(`error in DB ${error}`.bgRed.white);
+//     });
+// };
+// module.exports = connectDB;
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGOURL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
-    .then((con) => {
-      console.log(`connect to mongoDB ${con.connection.host}`.bgCyan);
-    })
-    .catch((error) => {
-      console.log(`error in DB ${error}`.bgRed.white);
     });
+    console.log(`connected to mongoDB database ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`error in DB ${error}`);
+  }
 };
+
 module.exports = connectDB;
