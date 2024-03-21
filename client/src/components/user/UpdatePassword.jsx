b6ab4c63191ae791a31b14ce37f296434a37e7ad
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-21 22:45:50 +0500

    only auth user update profile functionality completed

---
+++ client/src/components/user/UpdatePassword.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import "./UpdatePassword.css";
+import Loader from "../layout/loader/Loader";
+import MetaData from "../layout/MetaData";
+import { useSelector } from "react-redux";
+import LockOpenIcon from "@material-ui/icons/LockOpen";
+import LockIcon from "@material-ui/icons/Lock";
+import VpnKeyIcon from "@material-ui/icons/VpnKey";
+import { toast } from "react-toastify";
+import { serverURL } from "../../config/config";
+const UpdatePassword = () => {
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+  //   console.log(token, "value in token");
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const updatePasswordSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const token = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("token="))
+        .split("=")[1];
+
+      const res = await fetch(`${serverURL}/user/change`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          oldPassword,
+          newPassword,
+          confirmPassword,
+        }),
+      });
+      const data = await res.json();
+      if (data.success === true) {
+        toast.success(data.message);
+        return;
+      }
+    } catch (error) {
+      return toast.error(error);
+    }
+  };
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <MetaData title="Change Password" />
+          <div className="updatePasswordContainer">
+            <div className="updatePasswordBox">
+              <h2 className="updatePasswordHeading">Update Profile</h2>
+
+              <form
+                className="updatePasswordForm"
+                onSubmit={updatePasswordSubmit}
+              >
+                <div className="loginPassword">
+                  <VpnKeyIcon />
+                  <input
+                    type="password"
+                    placeholder="Old Password"
+                    required
+                    value={oldPassword}
+                    onChange={(e) => setOldPassword(e.target.value)}
+                  />
+                </div>
+
+                <div className="loginPassword">
+                  <LockOpenIcon />
+                  <input
+                    type="password"
+                    placeholder="New Password"
+                    required
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                  />
+                </div>
+                <div className="loginPassword">
+                  <LockIcon />
+                  <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    required
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  value="Change"
+                  className="updatePasswordBtn"
+                />
+              </form>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default UpdatePassword;
