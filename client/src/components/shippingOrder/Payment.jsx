0d339ce27021898b4b96640c3b5c8a77747d856b
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-28 13:14:28 -0700

    Payment UI completed

---
+++ client/src/components/shippingOrder/Payment.jsx
@@ -0,0 +1,182 @@
+import  { useRef } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+
+import { Typography } from "@material-ui/core";
+
+import {
+  CardNumberElement,
+  CardCvcElement,
+  CardExpiryElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+
+
+import "./Payment.css";
+import CreditCardIcon from "@material-ui/icons/CreditCard";
+import EventIcon from "@material-ui/icons/Event";
+import VpnKeyIcon from "@material-ui/icons/VpnKey";
+import { serverURL } from "../../config/config";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import CheckoutSteps from "./CheckoutSteps";
+import MetaData from "../layout/MetaData";
+import { orderActions } from "../../redux/orderDetails/newOrderSlice";
+
+const Payment = () => {
+  const orderInfo = JSON.parse(sessionStorage.getItem("orderInfo"));
+console.log(orderInfo,'value in orderInfo')
+  const dispatch = useDispatch();
+ const navigate=useNavigate()
+  const stripe = useStripe();
+  const elements = useElements();
+  const payBtn = useRef(null);
+
+  const { shippingInfo, cartItems } = useSelector((state) => state.cart);
+  const { currentUser } = useSelector((state) => state.user);
+console.log(shippingInfo,currentUser,'both infor')
+
+  const paymentData = {
+    amount: Math.round(orderInfo.totalPrice * 100),
+  };
+
+  const order = {
+    shippingInfo,
+    orderItems: cartItems,
+    itemsPrice: orderInfo.subtotal,
+    taxPrice: orderInfo.tax,
+    shippingPrice: orderInfo.shippingCharges,
+    totalPrice: orderInfo.totalPrice,
+  };
+
+const createOrder=async(order)=>{
+  try{
+dispatch(orderActions.CREATE_ORDER_REQUEST())
+const res = await fetch(`${serverURL}/order/create-order`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  credentials: "include",
+  body: JSON.stringify({
+    order
+  })
+});
+const data=await res.json()
+if(data.success===false){
+  toast.error(data.message)
+  return
+}
+if(data.success){
+  dispatch(orderActions.CREATE_ORDER_SUCCESS(data))
+  toast.success(data.message)
+  return
+}
+
+  }
+  catch(error){
+dispatch(orderActions.CREATE_ORDER_FAIL(error.message))
+  }
+}
+const yourApiKeyHere="sk_test_51LzNIqH7XEPQOoZkzQADN5PuguZbe2DU8cTVPx0bndcnPCgPSrYuZQAO4GJLUrGsWiqoHcsm1cM2WW2mP16CaWUE00jzm6pwGE"
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    payBtn.current.disabled = true;
+
+    try {
+      
+    
+    
+      const res = await fetch(`${serverURL}/payment/process`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${yourApiKeyHere}`
+        },
+        credentials: "include",
+        body:JSON.stringify(paymentData),
+      });
+      const data=await res.json()
+      console.log(data,'value in Data')
+      const client_secret = data.client_secret;
+
+      if (!stripe || !elements) return;
+
+      const result = await stripe.confirmCardPayment(client_secret, {
+        payment_method: {
+          card: elements.getElement(CardNumberElement),
+          billing_details: {
+            name: currentUser.name,
+            email: currentUser.email,
+            address: {
+              line1: shippingInfo.address,
+              city: shippingInfo.city,
+              state: shippingInfo.state,
+              postal_code: shippingInfo.pinCode,
+              country: shippingInfo.country,
+            },
+          },
+        },
+      });
+
+      if (result.error) {
+        payBtn.current.disabled = false;
+
+        toast.error(result.error.message);
+      } else {
+        if (result.paymentIntent.status === "succeeded") {
+          order.paymentInfo = {
+            id: result.paymentIntent.id,
+            status: result.paymentIntent.status,
+          };
+
+        //   dispatch(createOrder(order));
+        createOrder(order)
+
+          navigate("/success");
+        } else {
+          toast.error("There's some issue while processing payment ");
+        }
+      }
+    } catch (error) {
+      payBtn.current.disabled = false;
+   toast.error(error)
+    }
+  };
+
+
+  return (
+    <>
+      <MetaData title="Payment" />
+      <CheckoutSteps activeStep={2} />
+      <div className="paymentContainer">
+        <form className="paymentForm" onSubmit={(e) => submitHandler(e)}>
+          <Typography>Card Info</Typography>
+          <div>
+            <CreditCardIcon />
+            <CardNumberElement className="paymentInput" />
+          </div>
+          <div>
+            <EventIcon />
+            <CardExpiryElement className="paymentInput" />
+          </div>
+          <div>
+            <VpnKeyIcon />
+            <CardCvcElement className="paymentInput" />
+          </div>
+
+          <input
+            type="submit"
+            value={`Pay - ₹${orderInfo && orderInfo.totalPrice}`}
+            ref={payBtn}
+            className="paymentFormBtn"
+          />
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Payment;
