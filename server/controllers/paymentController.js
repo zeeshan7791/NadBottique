bd24bf2cf43d2e422987350288cfc870040200d2
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-30 23:17:15 -0700

    payment api correct data placed

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -1,37 +1,33 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-
-const processPayment=async(req,res,next)=>{
-
-    try{
-        const myPayment = await stripe.paymentIntents.create({
-            amount: req.body.amount,
-            currency: "inr",
-            metadata: {
-              company: "Ecommerce",
+const dotenv=require('dotenv')
+dotenv.config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const processPayment = async (req, res, next) => {
+    try {
+       
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: parseInt(req.body.amount),
+            currency: 'pkr',
+            automatic_payment_methods: {
+              enabled: true,
             },
-          });
-        
-          res
-            .status(200)
-            .json({ success: true, client_secret: myPayment.client_secret });
+          })
+       
+        res.status(200).json({ success: true, client_secret: paymentIntent.client_secret });
+    } catch (error) {
+        next(error);
     }
-    catch(error){
-        next(error)
-    }
-}
+};
 
 const sendStripeApiKey = async (req, res, next) => {
-    try{
-
+    try {
         res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+    } catch (error) {
+        next(error);
     }
-    catch(error){
-        next(error)
-    }
-  };
-module.exports={
-    processPayment,
-    sendStripeApiKey,
+};
 
-}
+module.exports = {
+    processPayment,
+    sendStripeApiKey
+};
