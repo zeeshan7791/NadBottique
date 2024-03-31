dc0cd6f22d337d99af5422e7bbaaf2f1b2acfc6c
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-31 11:54:58 -0700

    payment route updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import WebFont from "webfontloader";
@@ -20,18 +20,41 @@ import ForgotPassword from "./components/user/ForgotPassword";
 import Cart from "./components/cart/Cart";
 import Shipping from "./components/shippingOrder/Shipping";
 import ConfirmOrder from "./components/shippingOrder/ConfirmOrder";
-
+import { serverURL } from "./config/config";
+import Payment from "./components/shippingOrder/Payment";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import OrderSuccess from "./components/shippingOrder/OrderSuccess";
+import MyOrders from "./components/order/MyOrder";
+import OrderDetails from "./components/order/OrderDetails";
 const App = () => {
+  const { isAuthenticated } = useSelector((state) => state.user);
+  const [isStripeKey,setStripeApiKey]=useState("")
+  const getStripeKey=async()=>{
+    const res=await fetch(`${serverURL}/payment/stripeapikey`,{
+      method: "GET",
+      credentials: "include",
+    
+
+    })
+    const data=await res.json()
+   
+    setStripeApiKey(data.stripeApiKey)
+  }
   useEffect(() => {
     WebFont.load({
       google: {
         families: ["Roboto", "Droid Sans", "Chilanka"],
       },
     });
-  }, []);
-  const { isAuthenticated } = useSelector((state) => state.user);
+    if(isAuthenticated){
+   
+      getStripeKey()
+    }
 
-  return (
+      
+  }, [isAuthenticated]);
+ return (
     <>
       <BrowserRouter>
         <Header />
@@ -46,7 +69,18 @@ const App = () => {
           <Route path="/products" element={<Products />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
           <Route path="/shipping" element={<Shipping/>}></Route>
+          <Route path="/orders" element={<MyOrders/>}></Route>
+
+   {isStripeKey && (
+  <Route exact path="/process/payment" element={
+    <Elements stripe={loadStripe(isStripeKey)}>
+      <Payment />
+    </Elements>
+  } />
+)} 
+          <Route path="/success" element={<OrderSuccess/>}></Route>
           <Route path="/order/confirm" element={<ConfirmOrder/>}></Route>
+          <Route path="/order/:id" element={<OrderDetails/>}></Route>
           <Route path="/products/:keyword" element={<Products />}></Route>
           <Route path="/Search" element={<Search />}></Route>
           <Route path="/login" element={<LoginSignUp />}></Route>
