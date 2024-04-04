6371b4cff2fdba99449e090785a297d71fc1164f
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-04-04 16:37:01 -0700

    admin all users slice created

---
+++ client/src/redux/user/adminAllUserSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+ const adminAllUserSlice = createSlice({
+    name: "allUsers",
+    initialState: { allUsers: null, isError: null, isLoading: false },
+    reducers: {
+      ADMIN_ALLUSERS_REQUEST: (state) => {
+        state.isLoading = true;
+      },
+      ADMIN_ALLUSERS_SUCCESS: (state, action) => {
+        state.allUsers = action.payload;
+        state.isError = null;
+        state.isLoading = false;
+      },
+      ADMIN_ALLUSERS_FAIL: (state, action) => {
+        (state.isError = action.payload), (state.isLoading = null);
+      },
+    },
+  });
+  export const allUsersActions = adminAllUserSlice.actions;
+  
+  export default adminAllUserSlice.reducer;
